2e29822a19dd9392db0565e100ab791a6e969209
roro3164 <mornet.romain2@gmail.com>
2024-06-05 14:41:24 +0200

    tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { TagComponent } from './components/tag/tag.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NavbarComponent, LoginFormComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    NavbarComponent,
+    LoginFormComponent,
+    TagComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'memopus';
+  tags = ["Echec", "English", "Formation", "Geographie", "HTML", "Vocabulaire"];
 }
