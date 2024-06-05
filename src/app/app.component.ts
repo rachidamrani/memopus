b477bb25d7735337d62b20451fff5eac658afe2d
devbymoz <mzaoui.2409@gmail.com>
2024-06-05 11:48:46 +0200

    Composant carte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { CardComponent } from './components/card/card.component.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NavbarComponent, LoginFormComponent],
+  imports: [RouterOutlet, RouterLink, NavbarComponent, LoginFormComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
