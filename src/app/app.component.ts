dcee04c2f4b8e2b4e1c6da29261a9e007d975692
rachidamrani <rachid3alaoui@gmail.com>
2024-06-05 10:42:37 +0200

    ajout du navbar et login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, NavbarComponent, LoginFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login", component: LoginFormComponent
+    }
+];
